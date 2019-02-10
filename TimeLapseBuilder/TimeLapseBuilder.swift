1bc137b5c2561c745a7010cef90b31339f40bc41
Adam Jensen <acjensen@gmail.com>
2019-02-09 21:06:01 -0500

    Address deprecations and build warnings

--- TimeLapseBuilder/TimeLapseBuilder.swift
+++ TimeLapseBuilder/TimeLapseBuilder.swift
@@ -154,12 +154,12 @@ class TimeLapseBuilder: NSObject {
                         withPresentationTime: presentationTime
                     )
                     
-                    pixelBufferPointer.deinitialize()
+                    pixelBufferPointer.deinitialize(count: 1)
                 } else {
                     NSLog("error: Failed to allocate pixel buffer from pool")
                 }
                 
-                pixelBufferPointer.deallocate(capacity: 1)
+                pixelBufferPointer.deallocate()
             }
         }
         
